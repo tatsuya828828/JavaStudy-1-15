b521ece10986c4a8f4ec588f9de4b2cf6f6a9569
tatsuya828828 <kkjy011894@gmail.com>
2020-07-12 18:59:46 +0900

    [finish]15-06

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,50 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println(isValidPlayerName("AIUEO123"));
-		System.out.println(isValidPlayerName("Aiueo123"));
-		// ピリオドを使うことでピリオドに文字を適当に代入した際に同じか調べることができる
-		System.out.println("Java".matches("J.va"));
-		// アスタリスクは直前の文字の0回以上の繰り返しを意味する
-		System.out.println("ABBBBB".matches("AB*"));
-		System.out.println("Jaaaaava".matches("Ja*va"));
-
-		// 波括弧は、直前の文字の指定回数の繰り返しを表す(下の場合であればa~zの文字列を3回繰り返すということになる)
-		// 角括弧は、角括弧内に書かれた文字列のどれか1文字が当てはまるかを調べる(下の場合であればa~zのどれかが当てはまればtrue)
-		System.out.println("url".matches("[a-z]{3}"));
-
-		System.out.println("URL".matches("UR[LIN]"));
-
-		String s = "abc,def:ghi";
-		// splitメソッドを用いることで文字列の分割を行うこともできる
-		// 下の場合であれば、カンマかコロンの場所で分割することを表している
-		String[] words = s.split("[,:]");
-		for(String w: words) {
-			System.out.print(w +"->");
+	public static void main(String[] args) throws Exception {
+		// 現在日時を持つDateインスタンスの生成
+		// Dateクラスも基準日時からの経過ミリ秒で計算している
+		Date d = new Date();
+		System.out.println(d);
+		// currentTimeMillisメソッドを呼べばlong型で受け取ることができる
+		// 基準日時から経過したミリ秒をlong型の変数に代入する
+		long start = System.currentTimeMillis();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		// replaceAllメソッドを用いることで、文字列中でパターンに一致した箇所を全て別の文字列に置き換えることができる
-		// 下の場合であればbehをXに置き換えることをあらわす
-		String w = s.replaceAll("[beh]", "X");
-		System.out.println(w);
-		// 文字列の整形
-		// 文字列の書式整形にはformatメソッドを用いる
-		// 同時に出力も行いたい場合はprintfメソッドを用いる
-		// 第一引数には、雛形を指定する書式指定文字列と呼ばれるものを指定する
-		// その中の%記号の部分は、プレースホルダと呼ばれ第二引数以降で指定した具体的な値が順に流し込まれる
-		// プレースホルダは
-		// % 修飾(省略可) 桁(省略可) 型(必須)　といった形で書く
-		// 修飾: ,(3桁毎にカンマを入れる) 0(空き領域を0埋め) -(左寄せ) +(符号を強制表示)
-		// 桁: 表示桁数をする、n.mのような形式で指定した場合、全体n桁、小数点以下m桁での表示となる
-		// 型: d(整数) s(文字列) f(少数) b(真偽値)
-		// のような書き方で指定する
-		// プレースホルダは、全角文字が混ざると崩れてしまうこともあるので注意する
-		// 下であれば製品番号 第二引数(左寄せの文字列) 第三引数(0埋め2桁の整数)を表す
-		System.out.printf("製品番号%s-%02d", "SJV", 3);
-	}
+		long end = System.currentTimeMillis();
+		// 経過した時間の差を出すことで処理時間の計測を表すことができる
+		System.out.println("処理はにかかった時間は・・・"+ (end - start) +"ミリ秒でした");
 
-	public static boolean isValidPlayerName(String name) {
-		// matchesメソッドを用いることで文字列が引数で渡しているパターンを満たしているかを照合する処理をしてくれる
-		// またこの処理を、パターンマッチングという
-		// 引数で渡しているのは文字列パターンと呼ばれるものである
-		// これは文字列の形式的な条件を、正規表現という文法に従って記述したものである
-		return name.matches("[A-Z][A-Z0-9]{7}");
+		Calendar c = Calendar.getInstance();
+		// 年、月、日、時、分、秒の6つのint値を代入する
+		c.set(2019, 8, 22 , 1, 23, 45);
+		// 月の値を9に変更
+		c.set(Calendar.MONTH, 9);
+		// 6つのint値を使ってDateインスタンスを生成
+		Date d1 = c.getTime();
+		System.out.println(d1);
+
+		// Dateインスタンスからint値を生成
+		Date now = new Date();
+		c.setTime(now);
+		// 生成したインスタンスから年を取り出す
+		int y = c.get(Calendar.YEAR);
+		System.out.println("今年は"+ y +"年です");
+
+		// y=年 M=月 d=日 H=時 m=分 s=秒を表している
+		// 他にも K=時(0~11) a/p=午前/午後 E=曜日 などがある
+		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		// 文字列からDateインスタンスを生成
+		Date d2 = f.parse("2020/09/22 01:23:45");
+		System.out.println(d2);
+		// Dateインスタンスから文字列を生成
+		Date now2 = new Date();
+		String s = f.format(now2);
+		System.out.println("現在は"+ s +"です");
 	}
 }
